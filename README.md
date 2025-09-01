b267a550becae71b65be1057a0e0a4a1e7de08e7
DiogoBotton <diogo.luo@gmail.com>
2025-09-01 10:52:23 -0300

    Frufruzinho e atualizado readme.md

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ pnpm create vite@latest my-app -- --template react-ts
 # Sem typescript
 pnpm create vite@latest my-app -- --template react
 ```
+
+#### Artigo de apoio para criação de Web Component com React Js e Vite
+
+[Create a Web Component from a React Component](https://techblog.skeepers.io/create-a-web-component-from-a-react-component-bbe7c5f85ee6)
--- wc_renderer_next/src/app/page.tsx
+++ wc_renderer_next/src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Widget } from "@/components/Widget"
 
+export const revalidate = 60;
+
 export default function Home() {
   const widgets = [
     {
@@ -7,16 +9,22 @@ export default function Home() {
     },
     {
       src: "https://diogobotton.github.io/POC_WebComponents/widget.es.js", tag: "portfolio-widget", name: "Diogo Botton", githubUrl: "https://github.com/DiogoBotton"
+    },
+    {
+      src: "https://diogobotton.github.io/POC_WebComponents/widget.es.js", tag: "portfolio-widget", name: "Gabriel Dias", githubUrl: "https://github.com/gabrieldiasss"
     }
   ]
 
   return (
     <div>
       <h1>Home</h1>
-
-      {widgets.map((w, i) => (
-        <Widget key={i} src={w.src} tag={w.tag} name={w.name} githubUrl={w.githubUrl} />
-      ))}
+      <div className="flex justify-center">
+        <div className="flex flex-col justify-evenly align-middle h-[100vh] w-100">
+          {widgets.map((w, i) => (
+            <Widget key={i} src={w.src} tag={w.tag} name={w.name} githubUrl={w.githubUrl} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
